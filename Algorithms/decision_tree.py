3bb3df881f77ddcc0614d52388a433b24e69b055
yesman89 <yesman89@users.noreply.github.com>
2022-10-27 23:39:49 -0500

    Initial decision tree file

---
+++ Algorithms/decision_tree.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 27 23:37:19 2022
+
+@author: 19525
+"""
+
+# This will be used for decision tree algorithm
+import pandas as pd
+import numpy as np
+
